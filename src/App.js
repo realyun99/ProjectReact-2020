345f09e3cfdc1cc0ce362c122928f764a8ca3bf1
realyun99 <rhdtls1206@naver.com>
2020-12-13 14:43:48 +0900

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Link, Route, BrowserRouter,Switch} from 'react-router-dom';
-import "./App.css"
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 class Search extends Component{
   constructor(props){
@@ -24,10 +25,9 @@ class Search extends Component{
         <button onClick={this.handleClick}>search</button>
         <h3>{this.state.text}</h3>
       </div>
-    )
+    );
   }
 }
-
 class App extends Component {
   render(){
     return(
@@ -44,14 +44,57 @@ class App extends Component {
                 <Route path="/Info" component={Info}></Route>
               </Switch>
           </BrowserRouter>
+
         </header>
       </div>
     );
   }
 }
 
+class SimpleSlider extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1
+      
+    };
+    return (
+      <div>
+         <h2>태그 이미지</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+      </div>
+    );
+  }
+}
+
 function Info(){
-  return <div>Info : image</div>;
+  return (
+    <div>
+      <SimpleSlider/>
+    </div>
+    );
 }
 
 export default App;
